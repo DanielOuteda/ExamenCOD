c97c537dea4f52031081d148913c98a994f2eb95
doutedasolla <doutedasolla@gmail.com>
2019-11-29 13:37:59 +0100

    Format numeros

---
+++ src/examencod/ExamenCOD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examencod;
+
+/**
+ *
+ * @author doutedasolla
+ */
+public class ExamenCOD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        1º Lo primero que hago es usar format en numeros para ordenarlo todo. 
+        
+        
+        
+        
+        */
+    }
+    
+}
